5f7617645b3636060d4cb411074e1f5d5e13e96c
Nandita Shit <nanditashit1999@gmail.com>
2026-08-15 17:35:17 +0530

    first step done

---
+++ 01_tokenization/hello_world/main.py
@@ -0,0 +1,17 @@
+from openai import OpenAI
+
+client = OpenAI(
+    base_url="https://openrouter.ai/api/v1",
+    api_key="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",
+)
+
+response = client.chat.completions.create(
+    model="gpt-4o-mini",
+    messages =[
+        
+        {"role": "user",
+         "content": "Hey , do you know what is gen ai?"}
+    ]
+)
+
+print(response)
